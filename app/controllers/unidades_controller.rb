b2313dbaa80f1b4c6ab83bc708e062849d08edf8
Rafaellucasaz <Rafaellucasaz94@gmail.com>
2025-03-05 22:34:25 -0300

    add unidades crud

---
+++ app/controllers/unidades_controller.rb
@@ -0,0 +1,51 @@
+class UnidadesController < ApplicationController
+  before_action :set_unidade, only: %i[ show edit update destroy ]
+
+  def index
+    @unidades = Unidade.all
+  end
+
+  def show
+    @unidade = Unidade.find(params[:id])
+  end
+
+  def new
+    @unidade = Unidade.new
+  end
+
+  def create
+    @unidade = Unidade.new(unidade_params)
+    if @unidade.save
+      redirect_to unidades_path, notice: "Unidade registrada com sucesso !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @unidade = Unidade.find(params[:id])
+  end
+
+  def update
+    @unidade = Unidade.find(params[:id])
+    if @unidade.update(unidade_params)
+      redirect_to @unidade, notice: "Unidade atualizada com sucesso !"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @unidade.destroy
+    redirect_to unidades_path, notice: "Unidade deletada com sucesso !"
+  end
+
+  private
+    def set_unidade
+      @unidade = Unidade.find(params[:id])
+    end
+
+    def unidade_params
+      params.require(:unidade).permit(:nome)
+    end
+end
--- app/models/unidade.rb
+++ app/models/unidade.rb
@@ -1,3 +1,4 @@
 class Unidade < ApplicationRecord
-  validates :nome, presence: true, uniqueness: true
+  validates :nome, presence: { message: "O nome da unidade não pode ser vazio" },
+  uniqueness: { message: "Nome de unidade já em uso" }
 end
