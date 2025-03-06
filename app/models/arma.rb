cea8f4acc863cb74dfb207248f4fd82759d27a54
Rafaellucasaz <Rafaellucasaz94@gmail.com>
2025-03-05 22:39:15 -0300

    add armas crud

--- app/models/arma.rb
+++ app/models/arma.rb
@@ -1,6 +1,6 @@
 class Arma < ApplicationRecord
   has_many :movimentacaos
-  validates :modelo, :registro, presence: true
-  validates :registro, uniqueness: true
+  validates :modelo, presence: { message: "Modelo da arma não pode ser vazio" }
+  validates :registro, presence: { message: "Registro da arma não pode ser vazio" }, uniqueness: { message: "Esse registro já existe" }
   attribute :emprestada, :boolean, default: false
 end
