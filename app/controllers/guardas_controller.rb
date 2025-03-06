0e12ae7faf5a2a09b3a55ce8ee6af0ebc0fc61b4
Rafaellucasaz <Rafaellucasaz94@gmail.com>
2025-03-05 22:36:52 -0300

    add guardas crud

---
+++ app/controllers/guardas_controller.rb
@@ -0,0 +1,54 @@
+class GuardasController < ApplicationController
+  before_action :set_guarda, only: %i[ show edit update destroy ]
+
+  def index
+    @guardas = Guarda.all
+  end
+
+  def show
+    @guarda = Guarda.find(params[:id])
+    @equipe = Equipe.find(@guarda.equipe_id)
+  end
+
+  def new
+    @guarda = Guarda.new
+    @equipes = Equipe.all
+  end
+
+  def create
+    @guarda = Guarda.new(guarda_params)
+    if @guarda.save
+      redirect_to guardas_path, notice: "Guarda registrado com sucesso !"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @guarda = Guarda.find(params[:id])
+    @equipes = Equipe.all
+  end
+
+  def update
+    @guarda = Guarda.find(params[:id])
+    if @guarda.update(guarda_params)
+      redirect_to @guarda, notice: "Guarda atualizado com sucesso !"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @guarda.destroy
+    redirect_to guardas_path, notice: "Guarda deletado com sucesso !"
+  end
+
+  private
+    def set_guarda
+      @guarda = Guarda.find(params[:id])
+    end
+
+    def guarda_params
+      params.require(:guarda).permit(:nome, :matricula, :porte_arma, :equipe_id)
+    end
+end
--- app/models/guarda.rb
+++ app/models/guarda.rb
@@ -1,6 +1,7 @@
 class Guarda < ApplicationRecord
   belongs_to :equipe
   has_many :movimentacaos
-  validates :nome_completo, :matricula, :porte_arma, presence: true
-  validates :matricula, uniqueness: true
+  validates :nome, presence: { message: "Nome não pode ser vazio" }
+  validates :matricula, presence: { message: "Nº da matrícula não pode ser vazio" },  uniqueness: { message: "Nº de matrícula já existe" }
+  validates :porte_arma, presence: { message: "Nº de porte de arma não pode ser vazio" }
 end
